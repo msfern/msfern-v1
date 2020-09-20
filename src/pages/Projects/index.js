579002a8e5348ab77c15cbce752d200926876905
Stephanie Fernandes <steph3001@gmail.com>
2020-09-20 19:52:07 -0300

    Updated Projects page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -4,13 +4,47 @@ import './style.scss';
 
 function Projects() {
   const projects = {
+    evoxNews: {
+      name: 'News Portal',
+      image: './news.png',
+      description: 'A News portal application.',
+      techs: ['React', 'Fetch API', 'SCSS'],
+      github: 'https://github.com/msfern/evox-news',
+      live: '',
+    },
+    freelator: {
+      name: 'Freelator',
+      image: './freelator.png',
+      description:
+        'A simple Freelance project calculator made with vanilla Javascript.',
+      techs: ['HTML', 'SCSS', 'Javascript'],
+      github: 'https://github.com/msfern/freelator',
+      live: 'https://msfern.github.io/freelator/',
+    },
+    githubOrgs: {
+      name: 'Github Organizations',
+      image: './orgs.png',
+      description:
+        'An application that displays the Github information of organizations.',
+      techs: ['React', 'React Router', 'SCSS'],
+      github: 'https://github.com/msfern/github-orgs',
+      live: 'https://github-organization-search.netlify.app/',
+    },
+    customerFilter: {
+      name: 'Customer Filter',
+      image: './customers.png',
+      description: 'Application to filter provided customers by their region.',
+      techs: ['React', 'Axios', 'React Router'],
+      github: 'https://github.com/msfern/juntossomosmais-challenge',
+      live: '',
+    },
     office365: {
       name: 'Office365 Email',
       image: './office365.png',
       description: "A component based on Office365's email application.",
       techs: ['React', 'Axios'],
       github: 'https://github.com/msfern/encontact-challenge',
-      live: '',
+      live: 'https://msfern.github.io/encontact-challenge/',
     },
     emojo: {
       name: 'Emojo',
@@ -21,14 +55,6 @@ function Projects() {
       github: 'https://github.com/msfern/emojo',
       live: 'https://emojo.netlify.com/',
     },
-    // office365: {
-    //   name: 'Office365 Email',
-    //   image: './office365.png',
-    //   description: 'A pizza creating application, where the user can manage ingredients, recipes and stock.',
-    //   techs: ['React', 'Axios', 'Express', 'Firebase', 'SCSS'],
-    //   github: '',
-    //   live: ''
-    // },
   };
 
   return (
